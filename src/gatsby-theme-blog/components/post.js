ba2db9e86734c9e4ce231f9cf03047a207881b9e
Marcus Lyons <marcus.a.lyons@gmail.com>
2021-04-01 22:38:20 -0600

    feat: added a back link to posts to navigate back to /blog;

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -1,7 +1,8 @@
 import React from "react"
+import { Link } from "gatsby"
 import { Styled, css } from "theme-ui"
 
-import getShareImage from '@jlengstorf/get-share-image'
+import getShareImage from "@jlengstorf/get-share-image"
 import PostFooter from "../components/post-footer"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -18,33 +19,47 @@ const Post = ({
   previous,
   next,
 }) => {
-    const socialImage = getShareImage({
-        title: post.title,
-        tagline: post.excerpt,
-        cloudName: 'marcuslyons',
-        imagePublicID: 'marcuslyons/blog-post-card',
-        font: 'futura',
-        textColor: '232129'
-    })
-    return (
-        <Layout location={location} title={title}>
-        <SEO title={post.title} description={post.excerpt} socialImage={socialImage} />
-        <main>
-          <Styled.h1>{post.title}</Styled.h1>
-          <Styled.p
-            css={css({
-              fontSize: 1,
-              mt: -3,
-              mb: 3,
-            })}
-          >
-            {post.date}
-          </Styled.p>
-          <MDXRenderer>{post.body}</MDXRenderer>
-        </main>
-        <PostFooter {...{ previous, next }} />
-      </Layout>
-    )
+  const socialImage = getShareImage({
+    title: post.title,
+    tagline: post.excerpt,
+    cloudName: "marcuslyons",
+    imagePublicID: "marcuslyons/blog-post-card",
+    font: "futura",
+    textColor: "232129",
+  })
+  return (
+    <Layout location={location} title={title}>
+      <SEO
+        title={post.title}
+        description={post.excerpt}
+        socialImage={socialImage}
+      />
+      <Styled.a
+        as={Link}
+        css={{
+          color: `inherit`,
+          boxShadow: `none`,
+        }}
+        to={`/blog`}
+      >
+        back
+      </Styled.a>
+      <main>
+        <Styled.h1>{post.title}</Styled.h1>
+        <Styled.p
+          css={css({
+            fontSize: 1,
+            mt: -3,
+            mb: 3,
+          })}
+        >
+          {post.date}
+        </Styled.p>
+        <MDXRenderer>{post.body}</MDXRenderer>
+      </main>
+      <PostFooter {...{ previous, next }} />
+    </Layout>
+  )
 }
 
 export default Post
