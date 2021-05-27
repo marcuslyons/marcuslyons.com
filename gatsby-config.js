774353a6d3bdd8884cf96e260d041c27e057af39
Marcus Lyons <marcus.a.lyons@gmail.com>
2021-05-26 21:58:28 -0600

    feat: added gatsby-plugin-feed-mdx; added configuration for rss feed;

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,68 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.frontmatter.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    "/writing/" +
+                    edge.node.frontmatter.slug,
+                  guid:
+                    site.siteMetadata.siteUrl +
+                    "/writing/" +
+                    edge.node.frontmatter.slug,
+                })
+              })
+            },
+            query: `
+            {
+              allMdx(
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    frontmatter {
+                      slug
+                      title
+                      excerpt
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: "/rss.xml",
+            title: "home",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/writing/",
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-theme-blog`,
       options: {
         basePath: `/writing`,
@@ -33,6 +95,7 @@ module.exports = {
     title: `home`,
     author: `Marcus Lyons`,
     description: `The musings, rants, and learning of a developer trying to make sense of chaos.`,
+    siteUrl: `https://marcuslyons.com`,
     social: [
       {
         name: `twitter`,
