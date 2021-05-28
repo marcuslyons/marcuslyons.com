81caecdfadc0825bf383d1dd93d9b50140a3262b
Marcus Lyons <marcus.a.lyons@gmail.com>
2021-05-27 22:48:51 -0600

    fix: fixed rss-feed not having the html of the article;

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,76 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map((edge) => {
+                // console.log(`${edge.node.body}`)
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url:
+                    site.siteMetadata.siteUrl +
+                    "/writing/" +
+                    edge.node.frontmatter.slug,
+                  guid:
+                    site.siteMetadata.siteUrl +
+                    "/writing/" +
+                    edge.node.frontmatter.slug,
+                  custom_elements: [
+                    {
+                      content: [{ _attr: { type: "html" } }, edge.node.html],
+                    },
+                  ],
+                })
+              })
+            },
+            query: `
+            {
+              allMdx(
+                limit: 10,
+                sort: { order: DESC, fields: [frontmatter___date] },
+              ) {
+                edges {
+                  node {
+                    html
+                    frontmatter {
+                      slug
+                      title
+                      excerpt
+                      date
+                    }
+                  }
+                }
+              }
+            }
+          `,
+            output: "/rss.xml",
+            title: "home",
+            // optional configuration to insert feed reference in pages:
+            // if `string` is used, it will be used to create RegExp and then test if pathname of
+            // current page satisfied this regular expression;
+            // if not provided or `undefined`, all pages will have feed reference inserted
+            match: "^/writing/",
+          },
+        ],
+      },
+    },
+    {
       resolve: `gatsby-theme-blog`,
       options: {
         basePath: `/writing`,
