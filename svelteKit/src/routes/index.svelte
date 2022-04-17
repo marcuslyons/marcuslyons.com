347461ef130f24ab1b965bf73a54ee994868d3cd
Marcus Lyons <marcus.a.lyons@gmail.com>
2022-04-17 12:28:37 -0600

    wip: started migration to svelteKit

---
+++ svelteKit/src/routes/index.svelte
@@ -0,0 +1,22 @@
+<svelte:head>
+  <title>Home | marcuslyons.com</title>
+</svelte:head>
+
+<script>
+  import heroImage from '../content/assets/hero-image.png'
+</script>
+
+<main>
+  <div><img src={heroImage} alt="" class="hero-image"></div>
+    <h1>👋🏼 Hi, I'm Marcus!</h1>
+    <p>I love web development, design, devops, and reducing manual
+    workflows through automation; basically all the things!</p>
+    <p>Killing unnecessary policy, process, and beauracracy are my jam
+    though.</p>
+</main>
+
+<style>
+  .hero-image {
+    width: 25vw;
+  }
+</style>
---
+++ svelteKit/src/routes/writing/[hash].svelte
@@ -0,0 +1,7 @@
+<script>
+  export let data;
+</script>
+
+<article>
+  {data}
+</article>
