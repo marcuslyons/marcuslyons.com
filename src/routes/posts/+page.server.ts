799e645ee269645e4f21f4ed8945749a143d1e59
Marcus Lyons <10541922+marcuslyons@users.noreply.github.com>
2023-08-12 19:59:36 -0600

    chore: cleanup (#417)

--- src/routes/posts/+page.server.ts
+++
@@ -1,9 +0,0 @@
-// src/routes/+page.server.ts
-import { posts } from '$lib/server/posts';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	return {
-		posts // make posts available on the client
-	};
-};
