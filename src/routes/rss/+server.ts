7a57d8e7e7347afcdac522720c20dd1a02e2fba3
Marcus Lyons <marcus.a.lyons@gmail.com>
2023-08-18 09:53:05 -0600

    fix: typo

--- src/routes/rss/+server.ts
+++ src/routes/rss/+server.ts
@@ -9,7 +9,7 @@ export async function GET() {
     'Cache-Control': 'max-age=0, s-maxage=3600',
     'Content-Type': 'application/xml',
   }
-
+console.log({posts});
   const generateXML = content => `<rss xmlns:dc="https://purl.org/dc/elements/1.1/" xmlns:content="https://purl.org/rss/1.0/modules/content/" xmlns:atom="https://www.w3.org/2005/Atom" version="2.0">
   <channel>
     <title>${name}</title>
@@ -19,9 +19,10 @@ export async function GET() {
       <item>
             <title>${post.title}</title>
             <description>${post.excerpt}</description>
-            <link>${site}/posts/${post.slug}/</link>
+            <link>${site}/post/${post.slug}/</link>
             <pubDate>${new Date(post.date)}</pubDate>
-            <content:encoded>${post.preview}
+            <content:encoded>
+              ${post.previewText}
               <div style="margin-top: 50px; font-style: italic;">
                 <strong>
                   <a href="${site}/posts/${post.slug}">
